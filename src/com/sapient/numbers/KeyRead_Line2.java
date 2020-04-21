8fd968941b5f5be82093fe50f29c9b6833ada53b
abhskg <abhasgorain@gmail.com>
2020-04-21 14:27:03 +0530

    KeyRead_Line2 added to numbers package

--- src/com/sapient/numbers/KeyRead_Line2.java
+++ src/com/sapient/numbers/KeyRead_Line2.java
@@ -1,5 +1,99 @@
 package com.sapient.numbers;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class KeyRead_Line2 {
+	private int[] data;
+	private int size;
+
+
+	public KeyRead_Line2(int num) {
+		this.data = new int[num];
+		this.size = num;
+	}
+
+	public int[] getData() {
+		return data;
+	}
+
+	public void setData(int[] data) {
+		this.data = data;
+	}
+
+	public void displayData(int[] data) {
+		System.out.println(Arrays.toString(data));
+	}
+
+	public void readData(int[] input) {
+		Scanner Sc = new Scanner(System.in);
+		int temp;
+		for (int i = 0; i < this.size; i++) {
+			temp = Sc.nextInt();
+			this.data[i] = temp;
+		}
+		Sc.close();
+	}
+
+	public void sort() {
+		Arrays.sort(this.data);
+	}
+
+	public void customSort() {
+		this.quickSort(this.data, 0, size - 1);
+	}
+
+	public void quickSort(int arr[], int begin, int end) {
+		if (begin < end) {
+			int partitionIndex = partition(arr, begin, end);
+
+			quickSort(arr, begin, partitionIndex - 1);
+			quickSort(arr, partitionIndex + 1, end);
+		}
+	}
+
+	private int partition(int arr[], int begin, int end) {
+		int pivot = arr[end];
+		int i = (begin - 1);
+
+		for (int j = begin; j < end; j++) {
+			if (arr[j] <= pivot) {
+				i++;
+
+				int swapTemp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = swapTemp;
+			}
+		}
+
+		int swapTemp = arr[i + 1];
+		arr[i + 1] = arr[end];
+		arr[end] = swapTemp;
+
+		return i + 1;
+	}
+
+	@SuppressWarnings("static-access")
+	public void search(int key) {
+		this.binarySearch(this.data, 0, this.size - 1, key);
+	}
+
+	public static void binarySearch(int arr[], int first, int last, int key) {
+		int mid = (first + last) / 2;
+		while (first <= last) {
+			if (arr[mid] < key) {
+				first = mid + 1;
+			} else if (arr[mid] == key) {
+				System.out.println("Element is found at index: " + mid);
+				break;
+			} else {
+				last = mid - 1;
+			}
+			mid = (first + last) / 2;
+		}
+		if (first > last) {
+			System.out.println("Element is not found!");
+		}
+	}
 
 }
--- src/com/sapient/numbers/NumberOperator.java
+++ src/com/sapient/numbers/NumberOperator.java
@@ -1,5 +1,7 @@
 package com.sapient.numbers;
 
+import java.util.Scanner;
+
 public class NumberOperator {
 
 	public static void main(String[] args) {
@@ -9,6 +11,12 @@ public class NumberOperator {
 		cl1.calData();
 		System.out.println(cl1);
 
+		System.out.println("Enter the count for numbers : ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		KeyRead_Line2 kl2 = new KeyRead_Line2(num);
+
+		sc.close();
 	}
 
 }
